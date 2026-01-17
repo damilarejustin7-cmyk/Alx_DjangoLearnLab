bca02b455a149f54fafbb0bd642e8f8c6e7c2368
Damilare Adegbola <damilarejustin7@gmail.com>
2026-01-17 20:06:59 +0100

    Fix: correctly located and stage views and urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView
-
-from .models import Book, Library
-
+from .models import Book
+from .models import Library
 def list_books(request):
     books = Book.objects.select_related("author").all()
     # Uses the required template:
