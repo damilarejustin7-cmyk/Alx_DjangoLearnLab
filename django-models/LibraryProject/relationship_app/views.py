cd1f6cf80ab38cc1eedc83c441ec2b202a86006e
Damilare Adegbola <damilarejustin7@gmail.com>
2026-01-17 12:49:58 +0100

    added views.py to task 01

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render
-from django.views.generic.details import DetailView
-from .models import Book
-from .models import Library
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .models import Book, Library
 
 def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/book_list.html', context)
+    books = Book.objects.select_related("author").all()
+    # Uses the required template:
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_objects_name = 'Library'
-    
+    template_name = "relationship_app/library_details.html"
+    context_object_name = "library"
