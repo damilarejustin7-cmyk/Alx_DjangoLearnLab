69ef19352e484195afa149f6c85b7cbc0e822c51
Damilare Adegbola <damilarejustin7@gmail.com>
2026-01-18 11:39:49 +0100

    Worked on login,logout,registerviews and templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,10 @@ from django.views.generic.detail import DetailView
 from .models import Book 
 from .models import Library
 
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationFrom
+
+
 def list_books(request):
     books = Book.objects.all()
     return render(
@@ -16,3 +20,9 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+
+Class SignUpView(DetailView):
+    form_class = UserCreationForm
+    success_url = "/login/"
+    template_name = 'registration/signup.html'
