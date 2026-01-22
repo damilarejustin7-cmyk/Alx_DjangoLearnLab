0236f6442f7bc6e40609527908b63122d098653a
Damilare Adegbola <damilarejustin7@gmail.com>
2026-01-22 15:27:55 +0100

    Updated views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
+from .models import Book
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def view_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    return render(request, 'bookshelf/view_book.html', {'book': book})
+def book_list(request): 
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
-    
     pass
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request, pk):
-    
+    book = get_object_or_404(Book, pk=pk)
+    pass
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, pk):
     pass
