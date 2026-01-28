0d76273707f07b1b5058f45e160aedece627f16d
Damilare Adegbola <damilarejustin7@gmail.com>
2026-01-28 22:43:00 +0100

    Worked on task 02

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.viewsets import ModelViewSet
 
-# Change the name here to BookList
+# This handles only the GET (List) logic as requested in Task 1
 class BookList(generics.ListAPIView): 
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# This handles GET, POST, PUT, DELETE logic for Task 2
+class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
