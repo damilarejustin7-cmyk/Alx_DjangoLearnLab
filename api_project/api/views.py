19fc87c009c50213e7ae7af0b1a5106c11818031
Damilare Adegbola <damilarejustin7@gmail.com>
2026-01-28 23:01:58 +0100

    Uppercase fix on viewset

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -10,6 +10,6 @@ class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
 
 # This handles GET, POST, PUT, DELETE logic for Task 2
-class BookViewSet(Viewsets.ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
