5fa24c461d4b3e7d6aff1cd4f006f59ef31eeef9
Damilare Adegbola <damilarejustin7@gmail.com>
2026-01-29 00:12:42 +0100

    Worked on task 03

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render
 from rest_framework import viewsets, generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 # This handles only the GET (List) logic as requested in Task 1
 class BookList(generics.ListAPIView): 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can access
 
 # This handles GET, POST, PUT, DELETE logic for Task 2
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated | IsAdminUser] 
+    
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # This prefixes all api urls with 'api/' (e.g., /api/books/)
-    path('api/', include('api.urls')), 
+    path('admin/', admin.site.urls), # This prefixes all api urls with 'api/' (e.g., /api/books/)
+    path('api/', include('api.urls')), # Include the api app's URLs
+    path('api-token-auth/', obtain_auth_token), # Endpoint to obtain auth token
 ]
 
