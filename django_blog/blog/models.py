ea7b866da0a84a9068cf63b080e3325524180f0c
Damilare Adegbola <damilarejustin7@gmail.com>
2026-02-10 19:15:44 +0100

    Imported User

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -9,8 +9,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-
 
 
 
