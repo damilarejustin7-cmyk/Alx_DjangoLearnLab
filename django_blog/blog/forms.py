5d11afb98bfded567ad7a45c58e4a60489f39389
Damilare Adegbola <damilarejustin7@gmail.com>
2026-02-11 18:28:54 +0100

    added comments in forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,12 @@
 from django.forms import ModelForm
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
 
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
