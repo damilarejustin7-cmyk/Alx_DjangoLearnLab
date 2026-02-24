fad018a0bfb72ab97ae72bec5384dcd64468b0da
Damilare Adegbola <damilarejustin7@gmail.com>
2026-02-24 11:23:56 +0100

    Worked on Settings so i can redeploy to render

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5mhxq0lqc&!#9kn(@4_)-^hy&2_dy!==_j8sar5h+s00nshh(d')
+SECRET_KEY = os.environ.get('SECRET_KEY')#'django-insecure-5mhxq0lqc&!#9kn(@4_)-^hy&2_dy!==_j8sar5h+s00nshh(d')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -100,13 +100,10 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
